fcff3c187c85096d0847ef12911c807c61d55453
nhn1998 <nhnayeem1@gmail.com>
2022-08-22 21:38:52 +0600

    player expenses added

--- js/player-budget.js
+++ js/player-budget.js
@@ -2,5 +2,15 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
     const perPlayerInput = document.getElementById('per-player-input')
     const perPlayerValueString = perPlayerInput.value;
     const perPlayerValue = parseInt(perPlayerValueString);
-    console.log(perPlayerValue);
+    // orderlist count
+    const orderListContainer = document.getElementById('order-list');
+    const orderListContainerValue = orderListContainer.childElementCount;
+    // get player expenses
+    const playerExpenses= document.getElementById('player-expenses');
+    const playerExpensesValueString = playerExpenses.innerText;
+    const playerExpensesValue = parseInt(playerExpensesValueString);
+    // calculation
+    const calculatePerPlayer = orderListContainerValue * perPlayerValue;
+    playerExpenses.innerText = calculatePerPlayer;
+
 })
