31832c6378ac05ae912e48a40282cf486e1b67cc
nhn1998 <nhnayeem1@gmail.com>
2022-08-22 23:01:44 +0600

    finish

--- js/player-budget.js
+++ js/player-budget.js
@@ -14,3 +14,24 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
     playerExpenses.innerText = calculatePerPlayer;
 
 })
+document.getElementById('calculate-total').addEventListener('click',function(){
+    // expenses ammount
+    const playerExpenses = document.getElementById('player-expenses');
+    const playerExpensesValueString = playerExpenses.innerText;
+    const playerExpensesValue = parseInt(playerExpensesValueString);
+    // manager cost
+    const managerCost = document.getElementById('manager-input');
+    const managerCostValueString= managerCost.value;
+    const managerCostValue = parseInt(managerCostValueString);
+    // coach input value
+    const coachCost = document.getElementById('coach-input');
+    const coachCostValueString = coachCost.value;
+    const coachCostValue = parseInt(coachCostValueString);
+    // calculations
+    const total = playerExpensesValue + managerCostValue + coachCostValue;
+    // Total ammount
+    const totalAmmount = document.getElementById('total-ammount');
+    totalAmmount.innerText = total;
+    
+
+})
