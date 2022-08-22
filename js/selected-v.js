3439d4df62a649d7a622f6d3fe22324ba1ccb514
nhn1998 <nhnayeem1@gmail.com>
2022-08-22 19:57:46 +0600

    another part added

--- js/selected-v.js
+++ js/selected-v.js
@@ -1,8 +1,16 @@
 function getListItems(ListValue){
     const orderListContainer = document.getElementById('order-list');
+    
+    const orderlistContainerValue = orderListContainer.childElementCount;
+    if(orderlistContainerValue > 4){
+        alert('You Can Select Max Five Players');
+        return;
+    }
     const li = document.createElement('li');
     li.innerText = ListValue;
-    orderListContainer.appendChild(li)
+    orderListContainer.appendChild(li);
+    
+    
 }
 
 document.getElementById('messi-btn').addEventListener('click',function(){
@@ -11,6 +19,8 @@ document.getElementById('messi-btn').addEventListener('click',function(){
     const messiValue = messiId.innerText;
 
     getListItems(messiValue);
+    document.getElementById('messi-btn').disabled = true;
+    
 })
 document.getElementById('andres-btn').addEventListener('click',function(){
 
@@ -18,4 +28,38 @@ document.getElementById('andres-btn').addEventListener('click',function(){
     const andresValue = andresId.innerText;
 
     getListItems(andresValue);
+    document.getElementById('andres-btn').disabled = true;
+    
+})
+document.getElementById('mbappe-btn').addEventListener('click',function(){
+    const mbappeId = document.getElementById('mbappe');
+    const mbappeValue = mbappeId.innerText;
+
+    getListItems(mbappeValue);
+    document.getElementById('mbappe-btn').disabled = true;
+    
+})
+document.getElementById('mene-btn').addEventListener('click',function(){
+    const meneId = document.getElementById('mene');
+    const meneValue = meneId.innerText;
+
+    getListItems(meneValue);
+    document.getElementById('mene-btn').disabled = true;
+    
+})
+document.getElementById('eden-btn').addEventListener('click',function(){
+    const edenId = document.getElementById('eden');
+    const edenValue = edenId.innerText;
+
+    getListItems(edenValue);
+    document.getElementById('eden-btn').disabled=true;
+    
+})
+document.getElementById('pogba-btn').addEventListener('click',function(){
+    const pogbaId = document.getElementById('pogba');
+    const pogbaValue = pogbaId.innerText;
+
+    getListItems(pogbaValue);
+    document.getElementById('pogba-btn').disabled = true;
+    
 })
